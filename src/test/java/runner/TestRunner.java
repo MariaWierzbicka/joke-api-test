79b7133e86b34a0b26c2275bb936c4e052dfc48a
Maria Wierzbicka <maria.a.wierzbicka@gmail.com>
2026-02-04 18:04:01 +0100

    Add pom.xml and TestRunner

---
+++ src/test/java/runner/TestRunner.java
@@ -0,0 +1,18 @@
+package runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"src/test/resources/featureFiles"},
+        glue = {"steps"},
+        plugin = {
+                "pretty",
+                "json:target/cucumber-reports/cucumber.json",
+                "html:target/cucumber-reports/cucumber.html"
+        }
+)
+public class TestRunner {
+}
